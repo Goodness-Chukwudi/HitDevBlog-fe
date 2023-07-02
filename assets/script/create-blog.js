40c563f87a932c09423a9e00bf3c03efe53eccf7
Arinzelight <arinzelight2@gmail.com>
2023-07-02 15:10:24 +0100

    console log id and value of each paragraph

--- assets/script/create-blog.js
+++ assets/script/create-blog.js
@@ -9,6 +9,8 @@ function addEventListeners () {
 
 const blogTemplates = [];
 const textAreaElement = document.getElementById("exampleFormControlTextarea1");
+
+
 function addBlogParagraph (e) {
     const value = textAreaElement.value;
     const blogTemplate = `<p id = ${blogTemplates.length} >${value}</p>`
@@ -16,10 +18,30 @@ function addBlogParagraph (e) {
 
     displayNewParagraphs()
     e.preventDefault();
+    
 }
+    
 
 function displayNewParagraphs () {
     let template = blogTemplates.join(" ");
     document.getElementById("blog-content").innerHTML = template;
     textAreaElement.value = "";
+
+    // Add event listener to each paragraph
+    const paragraphs = document.querySelectorAll("#blog-content p");
+    paragraphs.forEach((paragraph) => {
+    paragraph.addEventListener("click", () => {
+    console.log(paragraph);
+  });
+});
+
 }
+
+
+
+
+
+
+// value.addEventListener("click", () => {
+//     console.log(`id: ${blogTemplate.length} value: ${value}`);
+// });
